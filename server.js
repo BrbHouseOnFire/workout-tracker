0019b4cdb4c9a7ace1ecb6b02d5ae1cb0d857616
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-02-02 23:34:18 -0600

    deploy mongodb on heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect('mongodb://wojo_host:wojojum1@ds045588.mlab.com:45588/heroku_7197wp8p', { useNewUrlParser: true, useFindAndModify: false});
 app.use(require('./routes/api.js'));
 app.use(require('./routes/html.js'));
 app.listen(PORT, () => {
